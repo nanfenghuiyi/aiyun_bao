32a15d0555e7b9347d81ef4dcf00ad2df5596822
rongruitao <ssdacs@qq.com>
2020-01-20 17:24:47 +0800

    进度条

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ import global_msg from "./utils/main"
 
 import 'normalize.css/normalize.css' // 重置的现代替代方法
 
-// import NProgress from 'nprogress' // 引入nprogress插件
-// import 'nprogress/nprogress.css'  // 这个nprogress样式必须引入
+import NProgress from 'nprogress' // 引入nprogress插件
+import 'nprogress/nprogress.css'  // 这个nprogress样式必须引入
 
 // Vue.use(ElementUI)
 Vue.use(element)
@@ -21,18 +21,18 @@ Vue.config.productionTip = false
 Vue.prototype.$global_msg = global_msg
 
 // 简单配置
-// NProgress.configure({showSpinner: false }) // 关闭进度环
+NProgress.configure({showSpinner: false }) // 关闭进度环
 
 router.beforeEach((to, from, next) => {
   // 路由发生变化修改页面title
   if (to.meta.title) {
     document.title = to.meta.title
   }
-  // NProgress.start()
+  NProgress.start()
   next()
 })
 router.afterEach(() => {
-  // NProgress.done()
+  NProgress.done()
 })
 
 new Vue({
